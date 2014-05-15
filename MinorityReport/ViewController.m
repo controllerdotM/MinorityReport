a7a9f09a2faff293b4634b8828f13707510b7fb8
Meredith Packham <m3r3dithe1ise@gmail.com>
2014-05-15 12:44:20 -0500

    Minority Report

--- MinorityReport/ViewController.m
+++ MinorityReport/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *myFutureLabel;
+@property (weak, nonatomic) IBOutlet UILabel *preCogsLabel;
+@property CGAffineTransform transform;
 
 @end
 
@@ -17,8 +20,34 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	self.transform = self.myFutureLabel.transform;
 }
+
+-(IBAction)onDrag:(UIPanGestureRecognizer *)panGestureRecognizer
+{
+
+    if (panGestureRecognizer.state == UIGestureRecognizerStateEnded) {
+        [UIView animateWithDuration:1.0 animations:^{
+            self.myFutureLabel.transform = self.transform;
+        }];
+    }
+        else {
+
+    CGPoint point;
+    point = [panGestureRecognizer translationInView:self.view];
+    self.myFutureLabel.transform = CGAffineTransformMakeTranslation(point.x, point.y);
+    point.x += self.myFutureLabel.center.x;
+    point.y += self.myFutureLabel.center.y;
+
+
+    if(CGRectContainsPoint(self.preCogsLabel.frame, point))
+    {
+        self.myFutureLabel.text = @"a ficticious and incriminating future";
+        self.myFutureLabel.backgroundColor = [UIColor orangeColor];
+        [self.myFutureLabel sizeToFit];
+    }
+        }
+    }
 
 - (void)didReceiveMemoryWarning
 {
